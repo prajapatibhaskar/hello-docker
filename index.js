b2c24a55371ab5108a043e59a03041772bf425e6
Bhaskar Prajapati <2996268@tcsgenailab.onmicrosoft.com>
2026-07-03 16:02:49 +0530

    Add index.html

--- index.js
+++
@@ -1,9 +0,0 @@
-const http = require("http");
-
-const server = http.createServer((req, res) => {
-  res.end("Hello from Docker!\n");
-});
-
-server.listen(3000, () => {
-  console.log("Server running");
-});
